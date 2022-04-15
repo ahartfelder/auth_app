5b52d1f7303b20940ef15da387e307bc13c0129b
ahartfelder <ahartfelder@gmail.com>
2022-04-15 20:34:20 -0300

    https protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'auth-app-ahart.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'auth-app-ahart.herokuapp.com', protocol: 'https' }
 
   config.action_mailer.delivery_method = :smtp
 
