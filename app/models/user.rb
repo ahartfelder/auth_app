cd51a2478cf76e2b56531eda2f9fb5b4261872fa
ahartfelder <ahartfelder@gmail.com>
2022-04-15 10:40:09 -0300

    bcrypt-user model

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: { case_sensitive: false}, length: { maximum: 105 }, format: { with: /^(.+)@(.+)$/, message: "Email invalid", multiline: true }
+  validates :password, length: { in: 6..20 }
+end
